6be87e2c4a9771ed1553eaabdd51eb2937daaf91
luxDuck <108199623+luxDuck@users.noreply.github.com>
2022-07-08 07:34:57 -0700

    Add files via upload

---
+++ scripts/effects.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+const redBlast = new Effect(40, 100, e => {
+    Draw.color(Color.valueOf("e56666"));
+    Lines.stroke(e.fout() * 2);
+    Lines.circle(e.x, e.y, 4 + e.finpow() * 20);
+
+    Draw.color(Color.valueOf("e56666"));
+    for(let i = 0; i < 8; i++){
+        Drawf.tri(e.x, e.y, 6, 40 * e.fout(), i*45);
+    }
+
+    Draw.color();
+    for(let i = 0; i < 8; i++){
+        Drawf.tri(e.x, e.y, 3, 12 * e.fout(), i*45);
+    }
+});
+
+};
