ce72f68cbe774a5ded2a5e681c5aa35865d32486
luxDuck <108199623+luxDuck@users.noreply.github.com>
2022-07-08 06:53:12 -0700

    Update blocks.js

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -48,25 +48,53 @@ const rip = extend(PowerTurret, "rip", {
 
 // crow
 
-const crowBeam = extend(SapBulletType, {
+const crowMountBeam = extend(SapBulletType, {
 	sapStrength: 0.87,
 	damage: 60,
 	lifetime: 20,
-	length: 250,
+	length: 273,
 	color: Color.valueOf("e56666"),
 	hitColor: Color.valueOf("e56666"),
-	statusDuration: 280,
 	width: 0.86
 });
 
-const crowBeamMount = multiTLib.newWeapon(crowBeam, "");
-unoMount.reloadTime = 45;
-unoMount.ammoPerShot = 5;
-unoMount.x = 2.75;
-unoMount.y = 2.75;
-unoMount.shootY = 13/4;
-unoMount.recoilAmount = 1;
-unoMount.range = 9 * 8;
-unoMount.title = "Uno"
+const crowMount = multiTLib.newWeapon(crowBeam, "publicmod-crowM");
+crowBeamMount.reloadTime = 45;
+crowBeamMount.ammoPerShot = 5;
+crowBeamMount.x = 0;
+crowBeamMount.y = 2.75;
+crowBeamMount.shootY = 13/4;
+crowBeamMount.recoilAmount = 1;
+crowBeamMount.range = 250;
+crowBeamMount.title = "Beam Mount"
+
+const crowWeapons = [crowMount];
+
+const crowBolt = extend(BasicBulletType, {
+    width: 15,
+    height: 15,
+
+    damage: 65,
+    speed: 3,
+    shrinkY: 0,
+    lifetime: 50,
+    pierce: false,
+    absorbable: true,
+
+    despawnEffect: Fx.hitMeltdown,
+    hitEffect: Fx.hitMeltdown,
+    frontColor: Color.valueOf("e78888"),
+    backColor: Color.valueOf("e56666"),
+});
+
+const crow = multiTurretLib.newMultiTurret("crow", crowWeapons, Items.lead, crowBolt, 80, 20, "Crow");
+crow.size = 2;
+crow.range = 15 * 8;
+crow.maxAmmo = 225;
+crow.ammoPerShot = 12;
+crow.recoil = 2;
+crow.reloadTime = 21;
+crow.requirements = ItemStack.with(Items.copper, 135, Items.lead, 40, Items.graphite);
+crow.category = Category.turret;
 
 // end luxDuck's content
