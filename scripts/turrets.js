d2a11a79709fcf40f60685accdb72f972941a2b1
luxDuck <108199623+luxDuck@users.noreply.github.com>
2022-07-07 19:13:09 -0700

    Add files via upload

---
+++ scripts/turrets.js
@@ -0,0 +1,46 @@
+// luxDuck's content
+
+// rip
+
+const ripBackfire = extend(BasicBulletType, {
+    width: 18,
+    height: 18,
+
+    damage: 30,
+    speed: 6,
+    spin: -3
+    lifetime : 20,
+    sprite: "large-bomb"
+    pierce: true,
+
+    despawnEffect: Fx.hitLancer,
+    frontColor: Color.valueOf("ffffff"),
+    backColor: Color.valueOf("ffaa5f"),
+});
+
+const ripDisk = extend(BasicBulletType, {
+    width: 18,
+    height: 18,
+
+    damage: 30,
+    speed: 6,
+    spin: 3
+    lifetime: 20,
+    sprite: "large-bomb"
+    pierce: true,
+
+    despawnEffect: Fx.hitLancer,
+    shootEffect: Fx.hitLancer
+    frontColor: Color.valueOf("ffffff"),
+    backColor: Color.valueOf("ffaa5f"),
+
+    despawned(b){
+        ripBackfire.create(b, b.x, b.y, b.rotation() - 180, 1, 1);
+    }
+});
+
+const rip = extend(PowerTurret, "rip", {
+    shootType: ripDisk,
+});
+
+// end luxDuck's content
